8709d43d30ebc33afdd23ce5567de3840a70ad3e
vrain jean baptiste <96755166+Jaybee-v@users.noreply.github.com>
2024-10-01 16:26:13 +0200

    modified: update with adding disconnect button

--- src/app/AuthButton.server.tsx
+++ src/app/AuthButton.server.tsx
@@ -4,12 +4,12 @@ import AuthButtonClient from "./AuthButton.client";
 
 export default async function AuthButton() {
   const session = await auth();
-  if (session && session.user) {
-    session.user = {
-      name: session.user.name,
-      email: session.user.email,
-    };
-  }
+  // if (session && session.user) {
+  //   session.user = {
+  //     name: session.user.name,
+  //     email: session.user.email,
+  //   };
+  // }
 
   return (
     <SessionProvider basePath={BASE_PATH} session={session}>
--- src/components/common/navbar/MobileNavbar.tsx
+++ src/components/common/navbar/MobileNavbar.tsx
@@ -13,8 +13,10 @@ import {
 } from "@/components/ui/drawer";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { Separator } from "@/components/ui/separator";
+import { signOut } from "next-auth/react";
+import { DoorClosed } from "lucide-react";
 
 interface MobileNavbarProps {
   session: Session;
@@ -22,6 +24,7 @@ interface MobileNavbarProps {
 
 export const MobileNavbar = ({ session }: MobileNavbarProps) => {
   const path = usePathname();
+  const router = useRouter();
   const lettersName =
     session.user.name.charAt(0) + "." + session.user.lastname.charAt(0);
   return (
@@ -34,64 +37,156 @@ export const MobileNavbar = ({ session }: MobileNavbarProps) => {
           <DrawerTitle>Are you absolutely sure?</DrawerTitle>
           <DrawerDescription>This action cannot be undone.</DrawerDescription>
         </DrawerHeader>
-        <DrawerFooter>
-          <nav className="">
-            <ul className="grid gap-2">
-              <li className="">
-                <Link href="/" className="w-full">
-                  <DrawerClose className="w-full">
-                    <Button
-                      className="w-full"
-                      variant={path === "/" ? "default" : "outline"}
-                    >
-                      Accueil
-                    </Button>
-                  </DrawerClose>
-                </Link>
-              </li>
-              <li className="">
-                <Link href="/lessons" className="w-full">
-                  <DrawerClose className="w-full">
-                    <Button
-                      className="w-full"
-                      variant={path === "/lessons" ? "default" : "outline"}
-                    >
-                      Planning
-                    </Button>
-                  </DrawerClose>
-                </Link>
-              </li>
-              <li className="">
-                <Link href="/account/riders" className="w-full">
-                  <DrawerClose className="w-full">
-                    <Button
-                      className="w-full"
-                      variant={
-                        path === "/account/riders" ? "default" : "outline"
-                      }
-                    >
-                      Cavaliers
-                    </Button>
-                  </DrawerClose>
-                </Link>
-              </li>
-              <Separator />
-              <li>
-                <Link href="/account" className="w-full">
-                  <DrawerClose className="w-full">
-                    <Button
-                      className="w-full"
-                      variant={path === "/account" ? "default" : "outline"}
-                    >
-                      Mon compte
-                    </Button>
-                  </DrawerClose>
-                </Link>
-              </li>
-            </ul>
-            {/* <UserMenu session={session} /> */}
-          </nav>
-        </DrawerFooter>
+        {session.user.role !== "rider" && (
+          <DrawerFooter>
+            <nav className="">
+              <ul className="grid gap-2">
+                <li className="">
+                  <Link href="/" className="w-full">
+                    <DrawerClose className="w-full">
+                      <Button
+                        className="w-full"
+                        variant={path === "/" ? "default" : "outline"}
+                      >
+                        Accueil
+                      </Button>
+                    </DrawerClose>
+                  </Link>
+                </li>
+                <li className="">
+                  <Link href="/lessons" className="w-full">
+                    <DrawerClose className="w-full">
+                      <Button
+                        className="w-full"
+                        variant={path === "/lessons" ? "default" : "outline"}
+                      >
+                        Planning
+                      </Button>
+                    </DrawerClose>
+                  </Link>
+                </li>
+                <li className="">
+                  <Link href="/account/riders" className="w-full">
+                    <DrawerClose className="w-full">
+                      <Button
+                        className="w-full"
+                        variant={
+                          path === "/account/riders" ? "default" : "outline"
+                        }
+                      >
+                        Cavaliers
+                      </Button>
+                    </DrawerClose>
+                  </Link>
+                </li>
+                <Separator />
+                <li>
+                  <Link href="/account" className="w-full">
+                    <DrawerClose className="w-full">
+                      <Button
+                        className="w-full"
+                        variant={path === "/account" ? "default" : "outline"}
+                      >
+                        Mon compte
+                      </Button>
+                    </DrawerClose>
+                  </Link>
+                </li>
+                <li>
+                  <Button
+                    variant="destructive"
+                    className="w-full gap-4"
+                    onClick={async () => {
+                      await signOut();
+                      router.push("/");
+                      // router.refresh();
+                      // await signIn();
+                    }}
+                  >
+                    <DoorClosed className="w-6 h-6" />
+                    Déconnexion
+                  </Button>
+                </li>
+              </ul>
+              {/* <UserMenu session={session} /> */}
+            </nav>
+          </DrawerFooter>
+        )}
+        {session.user.role === "rider" && (
+          <DrawerFooter>
+            <nav className="">
+              <ul className="grid gap-2">
+                <li className="">
+                  <Link href="/" className="w-full">
+                    <DrawerClose className="w-full">
+                      <Button
+                        className="w-full"
+                        variant={path === "/" ? "default" : "outline"}
+                      >
+                        Accueil
+                      </Button>
+                    </DrawerClose>
+                  </Link>
+                </li>
+                <li className="">
+                  <Link href="/lessons" className="w-full">
+                    <DrawerClose className="w-full">
+                      <Button
+                        className="w-full"
+                        variant={path === "/lessons" ? "default" : "outline"}
+                      >
+                        Mon club / Moniteur
+                      </Button>
+                    </DrawerClose>
+                  </Link>
+                </li>
+                <li className="">
+                  <Link href="/account/riders" className="w-full">
+                    <DrawerClose className="w-full">
+                      <Button
+                        className="w-full"
+                        variant={
+                          path === "/account/riders" ? "default" : "outline"
+                        }
+                      >
+                        Cavaliers
+                      </Button>
+                    </DrawerClose>
+                  </Link>
+                </li>
+                <Separator />
+                <li>
+                  <Link href="/account" className="w-full">
+                    <DrawerClose className="w-full">
+                      <Button
+                        className="w-full"
+                        variant={path === "/account" ? "default" : "outline"}
+                      >
+                        Mon compte
+                      </Button>
+                    </DrawerClose>
+                  </Link>
+                </li>
+                <li>
+                  <Button
+                    variant="destructive"
+                    className="w-full gap-4"
+                    onClick={async () => {
+                      await signOut();
+                      router.push("/");
+                      // router.refresh();
+                      // await signIn();
+                    }}
+                  >
+                    <DoorClosed className="w-6 h-6" />
+                    Déconnexion
+                  </Button>
+                </li>
+              </ul>
+              {/* <UserMenu session={session} /> */}
+            </nav>
+          </DrawerFooter>
+        )}
       </DrawerContent>
     </Drawer>
   );
